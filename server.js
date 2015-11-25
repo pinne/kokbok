1c8d3c19fa1a10885075c724beb2f57caa3a851d
Simon Kers <simon.kers@hiq.se>
2015-11-25 21:54:41 +0100

    Using express http server

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+app.use(express.static('app'));
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
+// respond with "Hello World!" on the homepage
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/app/index.html'));
+});
+
+// accept POST request on the homepage
+app.post('/', function (req, res) {
+  res.send('Got a POST request');
+});
+
+// accept PUT request at /user
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user');
+});
+
+// accept DELETE request at /user
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user');
+});
+
